23a4c11a5129f041b8b9e8a44435a368ecb1cfaf
cometasdepapel <104895262+cometasdepapel@users.noreply.github.com>
2023-02-19 23:55:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//Errores
+
+function validaNombre( nombre){
+
+    if(nombre!=null ){
+
+        document.nombre=true;
+    }
+
+}
+
+function validaContrasenia(password,cpassword){
+
+    if(password==cpassword){
+        //devolver exito
+        
+    }else{
+
+        //mostrar error
+    }
+}
+
+
